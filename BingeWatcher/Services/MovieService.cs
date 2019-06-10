9d5a24b1e95e18c11794e79d341a34eb1360efca
SomeFatBlackCat <SomeFatBlackCat@DESKTOP-BKBER74>
2019-06-10 21:29:53 +0200

    Dodanie komunikacji oraz klasy httpUrls

--- BingeWatcher/Services/MovieService.cs
+++ BingeWatcher/Services/MovieService.cs
@@ -1,10 +1,12 @@
 ﻿using BingeWatcher.Core.Models;
 using BingeWatcher.Core.Services;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Web.Http;
 
 namespace BingeWatcher.Services
 {
@@ -12,22 +14,66 @@ namespace BingeWatcher.Services
     {
         public async Task<List<Movie>> GetAllMovies()
         {
-            throw new NotImplementedException();
+            Uri uri = new Uri(HttpUrls.MOVIE_GET_ALL);
+            using (var httpClient = new HttpClient())
+            using (var response = await httpClient.GetAsync(uri))
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<List<Movie>>(result);
+                }
+                else
+                {
+                    throw new Exception("Nie udało się pobrać listy filmów!");
+                }
         }
 
         public async Task<Movie> GetMovieById(int movieId)
         {
-            throw new NotImplementedException();
+            Uri uri = new Uri(HttpUrls.MOVIE_GET_BY_ID);
+            using (var httpClient = new HttpClient())
+            using (var response = await httpClient.GetAsync(uri))
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<Movie>(result);
+                }
+                else
+                {
+                    throw new Exception("Nie udało się pobrać filmu!");
+                }
         }
 
         public async Task<List<Movie>> GetMoviesByGenre(MovieGenre genre)
         {
-            throw new NotImplementedException();
+            Uri uri = new Uri(HttpUrls.MOVIE_GET_BY_GENERE);
+            using (var httpClient = new HttpClient())
+            using (var response = await httpClient.GetAsync(uri))
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<List<Movie>>(result);
+                }
+                else
+                {
+                    throw new Exception("Nie udało się pobrać listy filmów!");
+                }
         }
 
         public async Task<List<Movie>> GetMoviesBySearchText(string searchText)
         {
-            throw new NotImplementedException();
+            Uri uri = new Uri(HttpUrls.MOVIE_GET_BY_SEARCH);
+            using (var httpClient = new HttpClient())
+            using (var response = await httpClient.GetAsync(uri))
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<List<Movie>>(result);
+                }
+                else
+                {
+                    throw new Exception("Nie udało się pobrać listy filmów!");
+                }
         }
     }
 }
