a4a604e0c74693a879e4f676a2a8a069046444d7
SomeFatBlackCat <SomeFatBlackCat@DESKTOP-BKBER74>
2019-09-10 19:17:16 +0200

    dodatkowe strony

---
+++ BingeWatcher/Views/MovieGeneresPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using BingeWatcher.ViewModels;
+
+using Windows.Media.Playback;
+using Windows.System.Display;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace BingeWatcher.Views
+{
+    public sealed partial class MovieGeneresPage : Page
+    {
+        private MovieGeneresViewModel ViewModel { get; } = ViewModelLocator.Current.MovieGeneresViewModel;
+
+        // For more on the MediaPlayer and adjusting controls and behavior see https://docs.microsoft.com/en-us/windows/uwp/controls-and-patterns/media-playback
+        // The DisplayRequest is used to stop the screen dimming while watching for extended periods
+        private readonly DisplayRequest _displayRequest = new DisplayRequest();
+        private bool _isRequestActive = false;
+
+        public MovieGeneresPage()
+        {
+            InitializeComponent();
+        }
+
+       
+        private async void PlaybackSession_PlaybackStateChanged(MediaPlaybackSession sender, object args)
+        {
+            if (sender is MediaPlaybackSession playbackSession && playbackSession.NaturalVideoHeight != 0)
+            {
+                if (playbackSession.PlaybackState == MediaPlaybackState.Playing)
+                {
+                    if (!_isRequestActive)
+                    {
+                        await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                        {
+                            _displayRequest.RequestActive();
+                            _isRequestActive = true;
+                        });
+                    }
+                }
+                else
+                {
+                    if (_isRequestActive)
+                    {
+                        await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                        {
+                            _displayRequest.RequestRelease();
+                            _isRequestActive = false;
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
