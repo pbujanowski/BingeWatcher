7414353fcfd1e4e6b14980e58897fbd3fd35d386
myszaaa <myszaaa@users.noreply.github.com>
2019-09-10 19:15:31 +0200

    SearchPage

---
+++ BingeWatcher/Views/SearchPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace BingeWatcher.Views
+{
+    public sealed partial class SearchPage : ContentDialog
+    {
+        public SearchPage()
+        {
+            // TODO WTS: Update the contents of this dialog with any important information you want to show when the app is used for the first time.
+            RequestedTheme = (Window.Current.Content as FrameworkElement)?.RequestedTheme ?? ElementTheme.Default;
+            InitializeComponent();
+        }
+    }
+}
